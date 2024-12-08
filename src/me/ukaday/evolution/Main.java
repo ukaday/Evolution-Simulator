db815bdc1c54636da1fc65b08f72307c11eab8bd
ukaday <hayhaygrogh@gmail.com>
2024-12-07 20:26:41 -0600

    fix main.java

--- src/me/ukaday/evolution/Main.java
+++ src/me/ukaday/evolution/Main.java
@@ -2,7 +2,8 @@ package me.ukaday.evolution;
 
 public class Main {
 
-    public static void main( String args[] ) {
+    public static void main(String[] args) {
         new Evolution();
     }
+
 }
